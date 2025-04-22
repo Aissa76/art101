c61b75cd6460155715970e109b3f12f17e8105e8
Aissa76 <abenmimo@ucsc.edu>
2025-04-21 21:40:15 -0700

    added pseudo coding and usual lab programming. Also aded pseudocode to JS.

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,3 +1,17 @@
+/**
+ * Summary. A simplified Battleship game (player vs computer).
+ *
+ * Description. This file contains the pseudocode and eventual implementation
+ * of a simplified Battleship game using a 5x5 grid and randomly placed ships.
+ *
+ * @link   https://aissa76.github.io/art101/lab4/index.html
+ * @file   This file contains pseudocode for a Battleship game.
+ * @author Aissa Ben-Mimoun & Audrey DeMerchant.
+ * @since  1.0.0
+ */
+
+/** jshint esversion: 6 */
+
 // index.js - purpose and description here
 // Author: Your Name
 // Date:
@@ -19,3 +33,26 @@ function main() {
 
 // let's get this party started
 main();
+//set the board 
+//create game boards for both player and computer
+//player places 3 ships randomly on board
+//computer places 3 ships randomly on board
+//play the game 
+//repeat until one player loses all ships
+//players turn
+                  	//display computer board (only show hits/misses, not ship locations)
+//ask player to enter target coordinates (e.g., B3)
+//accept user input (guess) coords 
+//check if shot is a hit or miss
+//if hit → mark board, decrease computer’s ship count if sunk
+//if miss → mark board
+	//computers turn 
+		//randomly select target coordinates not already used
+		//check if shot is a hit or miss
+//if hit → mark board, decrease players ship count if sunk
+//if miss → mark board
+//if hit all spots of ship → display ship on board
+//end game
+	//if player has 0 ships → display "You Lost!"
+	//if computer has 0 ships → display "You Won!"
+	//ask player to play again
