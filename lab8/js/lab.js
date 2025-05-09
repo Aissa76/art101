6890bbe82bf40492679e5c32cf88c0c6a9172009
Aissa76 <abenmimo@ucsc.edu>
2025-05-08 22:20:46 -0700

    creation of lab 8 including all new files and folders of html,css,js

---
+++ lab8/js/lab.js
@@ -0,0 +1,37 @@
+// index.js - purpose and description here
+// Author: Aissa Ben-Mimoun
+// Date: may 8
+//license public Domain
+
+// Constants
+let chicken = [1, 2, 3, 4, 5];
+let soup = ['a', 'b', 'c', 'd', 'e'];
+let word = "Valarie";
+
+// Functions
+function yourFunctionName(x){
+  var results = x*x;
+  return results;
+  }
+
+  console.log(yourFunctionName(5));
+  console.log(chicken.map(yourFunctionName));
+  console.log(word);
+
+  chicken.map(function(x){
+    var results = x*x;
+    return results;
+  });
+
+  var mapResults = chicken.map(yourFunctionName);
+  console.log("Results:", mapResults);
+
+  
+  function main() {
+    console.log("Main function started.");
+    // the code that makes everything happen
+  }
+  
+  // let's get this party started
+  main();
+  
