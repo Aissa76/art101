ac18cd32ba867aea92dea7791d1adc429603ec6e
Aissa76 <abenmimo@ucsc.edu>
2025-05-08 22:28:32 -0700

    js edits

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -1,7 +1,9 @@
-// index.js - purpose and description here
-// Author: Aissa Ben-Mimoun
-// Date: may 8
-//license public Domain
+ /*
+ * index.js - purpose and description here
+ * Author: Aissa Ben-Mimoun
+ * Date: may 8
+ *license public Domain
+ */
 
 // Constants
 let chicken = [1, 2, 3, 4, 5];
@@ -12,22 +14,22 @@ let word = "Valarie";
 function yourFunctionName(x){
   var results = x*x;
   return results;
-  }
+}
 
-  console.log(yourFunctionName(5));
-  console.log(chicken.map(yourFunctionName));
-  console.log(word);
+console.log(yourFunctionName(5));
+console.log(chicken.map(yourFunctionName));
+console.log(word);
 
-  chicken.map(function(x){
-    var results = x*x;
-    return results;
-  });
+chicken.map(function(x){
+  var results = x*x;
+  return results;
+});
 
-  var mapResults = chicken.map(yourFunctionName);
-  console.log("Results:", mapResults);
+var mapResults = chicken.map(yourFunctionName);
+console.log("Results:", mapResults);
 
-  
-  function main() {
+
+function main() {
     console.log("Main function started.");
     // the code that makes everything happen
   }
